849c5205c904e45f4144000578c66c19067b90d8
bcf112 <bcf112@naver.com>
2012-07-26 11:48:29 +0900

    init

---
+++ src/Lotto.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+
+public class Lotto {
+
+	/**
+	 * @param args
+	 *            인스턴스 변수
+	 * 
+	 */
+	Random random = new Random();
+
+	public static void main(String[] args) {
+		new Lotto().printLotto();
+	}
+
+	public void printLotto() {
+		for (int i = 0; i < 6; i++) {
+			System.out.println(getNumber(random));
+		}
+	}
+
+	private int getNumber(Random random) {
+		return random.nextInt(45) + 1;
+	}
+
+}
